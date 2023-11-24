c6b8406d78c48405ae120d50ed4ddbe3a08a9dda
Gourisankar G <kgourisankar01@gmail.com>
2023-11-24 12:53:58 +0530

    introduced animation, developed project section

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// JavaScript to trigger the animation when the element comes into view
+const animateOnScroll = document.querySelectorAll('.animate-on-scroll');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        }
+    });
+}, { threshold: 0.8 }); // Adjust the threshold as needed
+
+animateOnScroll.forEach(element => {
+    observer.observe(element);
+});
